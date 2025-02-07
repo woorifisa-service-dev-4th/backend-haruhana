182adbe8e135065d518109a04cc45f43253f3815
namsh1125 <namsh1125@naver.com>
2025-02-07 23:55:35 +0900

    🎉 프로젝트 초기 설정

---
+++ src/main/java/site/haruhana/www/HaruhanaApplication.java
@@ -0,0 +1,13 @@
+package site.haruhana.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruhanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaruhanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/haruhana/www/HaruhanaApplicationTests.java
@@ -0,0 +1,13 @@
+package site.haruhana.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruhanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
