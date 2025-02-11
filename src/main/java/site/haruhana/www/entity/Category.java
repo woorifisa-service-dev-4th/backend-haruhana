8997b57ff5a7a8b4e68658fa6e5941199b488d6f
saebomnewspring <sae051@hufs.ac.kr>
2025-02-12 03:23:10 +0900

    ♻️ Category enum 수정

--- src/main/java/site/haruhana/www/entity/Category.java
+++ src/main/java/site/haruhana/www/entity/Category.java
@@ -1,10 +1,10 @@
 package site.haruhana.www.entity;
 
 public enum Category {
-    backend,
-    frontend,
-    network,
-    operating_system,
-    data_structure,
-    data_base
+    BACKEND,
+    FRONTEND,
+    NETWORK,
+    OPERATING_SYSTEM,
+    DATA_STRUCTURE,
+    DATABASE
 }
