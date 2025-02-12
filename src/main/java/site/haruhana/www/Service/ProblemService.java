dd39e12901412984dd675e1687117ca23a5c0cf0
saebomnewspring <sae051@hufs.ac.kr>
2025-02-12 16:37:58 +0900

    ✨ ProblemService 로그 추가

--- src/main/java/site/haruhana/www/Service/ProblemService.java
+++ src/main/java/site/haruhana/www/Service/ProblemService.java
@@ -1,6 +1,7 @@
 package site.haruhana.www.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import site.haruhana.www.repository.ProblemRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProblemService {
@@ -23,6 +25,7 @@ public class ProblemService {
      * 모든 문제 검색하여 반환
      */
     public List<Problem> getAllProblems() {
+        log.info("Fetching all problems from the database");
         return problemRepository.findAll();
     }
 
@@ -30,7 +33,9 @@ public class ProblemService {
      * 주어진 ID의 문제를 dto 로 변환하여 반환
      */
     public ProblemDto getProblemById(Long id) {
+        log.info("Fetching problem with ID: {}", id);
         Problem problem = problemRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Problem not found"));
+        log.error("Problem not found with ID: {}", id);
         // 생성자를 사용하여 ProblemDto 객체 생성
         return new ProblemDto(problem.getId(), problem.getTitle(), problem.getDescription(), problem.getLevel(), problem.getProblemCategory());
     }
@@ -40,6 +45,7 @@ public class ProblemService {
      * 문제 생성
      */
     public Problem createProblem(Problem problem) {
+        log.info("Creating new problem with title: {}", problem.getTitle());
         return problemRepository.save(problem);
     }
 
@@ -48,6 +54,7 @@ public class ProblemService {
      */
     @Transactional
     public Problem updateProblem(Long id, Problem updatedProblem) {
+        log.info("Updating problem with ID: {}", id);
         Problem problem = problemRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Problem not found"));
 
@@ -67,6 +74,7 @@ public class ProblemService {
      * 문제 삭세
      */
     public void deleteProblem(Long id) {
+        log.info("Deleting problem with ID: {}", id);
         problemRepository.deleteById(id);
     }
 
@@ -78,6 +86,7 @@ public class ProblemService {
      * @return 해당 카테고리에 속하는 문제들의 페이지
      */
     public Page<Problem> getProblemsByCategory(ProblemCategory problemCategory, Pageable pageable) {
+        log.info("Fetching problems by category: {}", problemCategory);
         return problemRepository.findByProblemCategory(problemCategory, pageable);
     }
 }
