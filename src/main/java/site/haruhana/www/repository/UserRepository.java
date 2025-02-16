d06a29c1bf9b4dae844418a1375ad683ec74a5cc
namsh1125 <namsh1125@naver.com>
2025-02-16 21:31:34 +0900

    ✨ 인증 제공자와 이메일로 사용자가 존재하는지 확인하는 Repository단 구현

--- src/main/java/site/haruhana/www/repository/UserRepository.java
+++ src/main/java/site/haruhana/www/repository/UserRepository.java
@@ -9,6 +9,15 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
+     * 인증 제공자와 이메일로 사용자가 존재하는지 확인합니다.
+     *
+     * @param provider 인증 제공자 (GOOGLE, KAKAO, NAVER 등)
+     * @param email    조회할 사용자의 이메일
+     * @return 사용자가 존재하는지 여부
+     */
+    boolean existsByProviderAndEmail(AuthProvider provider, String email);
+
+    /**
      * 인증 제공자와 이메일로 사용자를 조회합니다.
      *
      * @param provider 인증 제공자 (GOOGLE, KAKAO, NAVER 등)
